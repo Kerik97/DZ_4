c0ceb696d21ac00f028642fd9778ef3ba25af56e
kirill <k.grinikh@gmail.com>
2022-09-13 10:42:36 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,17 @@
+﻿// 29 Задать массив из 8 элементов и вывести их на экран
+
+//int[] mas = { 1, 2, 3, 4, 5, 6, 7, 8 };
+//void function(int[] array)
+//{
+  //  for (int i = 0; i < array.Length; i++)
+    //    Console.Write(array[i] + " ");
+//}
+//function(mas);
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine((array[i]));
+    Console.Write((array[i]) + " ");
+}
+
